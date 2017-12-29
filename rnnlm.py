5742fb06a4d48bc2ba80f12e0ec00f0ec9928de7
Minato Sato <0zw3253t2c0390a@gmail.com>
2017-12-30 08:47:07 +0900

    fix bug (incorrect reshape timing)

--- rnnlm.py
+++ rnnlm.py
@@ -35,7 +35,7 @@ from tqdm import tqdm
 
 """cuda setting"""
 from nnabla.contrib.context import extension_context
-ctx = extension_context("cuda.cudnn", device_id=1)
+ctx = extension_context('cuda.cudnn', device_id=0)
 nn.set_default_context(ctx)
 """"""
 
@@ -76,13 +76,13 @@ from keras.preprocessing import sequence
 
 train_data = load_data('./ptb/train.txt')
 train_data = data2sentences(train_data)
-train_data = sequence.pad_sequences(train_data, padding="post")
+train_data = sequence.pad_sequences(train_data, padding='post')
 
 vocab_size = len(w2i)
-sentence_length = 40
-embedding_size = 512
-hidden = 512
-batch_size = 64
+sentence_length = 20
+embedding_size = 128
+hidden = 128
+batch_size = 256
 max_epoch = 100
 
 x_train = train_data[:, :sentence_length].astype(np.int32)
@@ -93,9 +93,9 @@ num_batch = len(x_train)//batch_size
 def load_func(index):
     return x_train[index], y_train[index]
 
-data_iter = data_iterator_simple(load_func, len(x_train), batch_size, shuffle=False, with_file_cache=True)
+data_iter = data_iterator_simple(load_func, len(x_train), batch_size, shuffle=True, with_file_cache=False)
 
-def SimpleRNN(inputs, units, return_sequences=False):
+def SimpleRNN(inputs, units, h0, return_sequences=False):
     '''
     A vanilla recurrent neural network layer
     Args:
@@ -112,26 +112,41 @@ def SimpleRNN(inputs, units, return_sequences=False):
     batch_size = inputs.shape[0]
     sentence_length = inputs.shape[1]
 
-    inputs = F.split(inputs, axis=1)
+    inputs = F.split(inputs, axis=1) # split in the direction of sequence
 
-    h = nn.Variable.from_numpy_array(np.zeros((batch_size, units)))
+    h = h0
     for x in inputs:
         with nn.parameter_scope('x2h'):
             x2h = PF.affine(x, units, with_bias=False)
         with nn.parameter_scope('h2h'):
             h2h = PF.affine(h, units)
         h = F.tanh(x2h + h2h)
+        if return_sequences:
+            h = F.reshape(h, (batch_size, 1, units))
         hs.append(h)
 
     if return_sequences:
-        hs = F.concatenate(*hs, axis=0)
+        hs = F.concatenate(*hs, axis=1)
         hs = F.reshape(hs, (batch_size, sentence_length, units))
     else:
         return hs[-1]
     return hs
 
 def TimeDistributedAffine(x, units, name='output'):
-    return PF.affine(x, units, base_axis=2, name=name)
+    '''
+    A time distributed softmax crossentropy
+    Args:
+        x (nnabla.Variable): A shape of [B, SentenceLength, EmbeddingSize]
+    Returns:
+        nn.Variable: A shape [B, SentenceLength, units].
+    '''
+    # return PF.affine(x, units, base_axis=2, name=name)
+    ret = []
+    batch_size = x.shape[0]
+    for x_ in F.split(x, axis=1):
+        ret.append(F.reshape(PF.affine(x_, units, name=name), (batch_size, 1, units)))
+    return F.concatenate(*ret, axis=1)
+
 
 def TimeDistributedSoftmaxCrossEntropy(y_pred, y_true):
     '''
@@ -142,7 +157,6 @@ def TimeDistributedSoftmaxCrossEntropy(y_pred, y_true):
     Returns:
         nn.Variable: A shape [B, SentenceLength].
     '''
-
     ret = []
     for y_p, y_t in zip(F.split(y_pred, axis=1), F.split(y_true, axis=1)):
         ret.append(F.softmax_cross_entropy(y_p, y_t))
@@ -150,21 +164,26 @@ def TimeDistributedSoftmaxCrossEntropy(y_pred, y_true):
 
 x = nn.Variable((batch_size, sentence_length))
 t = nn.Variable((batch_size, sentence_length, 1))
+h0 = nn.Variable((batch_size, hidden))
 h = PF.embed(x, vocab_size, embedding_size)
-h = SimpleRNN(h, hidden, return_sequences=True)
-y = TimeDistributedAffine(h, vocab_size)
+h = SimpleRNN(h, hidden, h0, return_sequences=True)
+h = TimeDistributedAffine(h, hidden, name='hidden')
+y = TimeDistributedAffine(h, vocab_size, name='output')
 
-entropy = TimeDistributedSoftmaxCrossEntropy(y, t) * F.sum(F.sign(t), axis=2) # do not predict 'pad'.
-count = F.sum(F.sum(F.sign(t), axis=2), axis=1)
-loss = F.mean(F.sum(entropy, axis=1) / count)
+mask = F.sum(F.sign(t), axis=2) # do not predict 'pad'.
+entropy = TimeDistributedSoftmaxCrossEntropy(y, t) * mask
+count = F.sum(mask, axis=1)
+loss = F.mean(F.div2(F.sum(entropy, axis=1), count))
 
 # Create solver.
 solver = S.Momentum(1e-2, momentum=0.9)
+solver.set_parameters(nn.get_parameters())
 
 # Create monitor.
 from nnabla.monitor import Monitor, MonitorSeries, MonitorTimeElapsed
-monitor = Monitor('./tmp-lstm')
-monitor_loss = MonitorSeries("Training loss", monitor, interval=1)
+monitor = Monitor('./tmp-rnnlm')
+monitor_perplexity = MonitorSeries('perplexity', monitor, interval=1)
+
 
 for epoch in range(max_epoch):
     loss_set = []
@@ -173,14 +192,15 @@ for epoch in range(max_epoch):
         y_batch = y_batch.reshape(list(y_batch.shape) + [1])
 
         x.d, t.d = x_batch, y_batch
+        h0.d = 0
 
-        solver.set_parameters(nn.get_parameters(), reset=False, retain_state=True)
+        loss.forward(clear_no_need_grad=True)
         loss.forward()
-        loss_set.append(np.exp(loss.d.copy()))
+        loss_set.append(loss.d.copy())
         solver.zero_grad()
         loss.backward(clear_buffer=True)
         solver.update()
-    monitor_loss.add(epoch, np.mean(loss_set))
+    monitor_perplexity.add(epoch+1, np.e**np.mean(loss_set))
 
 
 
