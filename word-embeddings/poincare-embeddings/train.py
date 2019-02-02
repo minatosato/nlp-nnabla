dfa332593c1225d19bfdbbf487427309ca4f1731
Minato Sato <sato.minato@ohsuga.is.uec.ac.jp>
2019-02-02 11:44:28 +0000

    WIP backup

--- word-embeddings/poincare-embeddings/train.py
+++ word-embeddings/poincare-embeddings/train.py
@@ -24,6 +24,12 @@ from common.parametric_functions import lstm
 from common.functions import time_distributed
 from common.functions import time_distributed_softmax_cross_entropy
 from common.functions import get_mask
+from common.functions import expand_dims
+
+from common.trainer import Trainer
+
+from nnabla.utils.data_source_loader import download
+from nnabla.utils.data_source_loader import get_data_home
 
 from typing import Dict, Union
 
@@ -41,11 +47,53 @@ if args.context == 'cudnn':
     nn.set_default_context(ctx)
 
 
-def distance(u, v):
+"""
+"""
+
+
+embedding_size: int = 2
+batch_size: int = 4
+max_epoch: int = 100
+negative_sample_size = 10
+
+file_url = 'https://raw.githubusercontent.com/qiangsiwei/poincare_embedding/master/data/mammal_subtree.tsv'
+
+from functools import reduce
+import operator
+import random
+
+with download(file_url, open_file=True) as f:
+    lines: str = f.read().decode('utf-8').split('\n')
+    pdata = list(map(lambda l:l.split('\t'),filter(None,lines)))
+
+pdict = {w:i for i,w in enumerate(set(reduce(operator.add, pdata)))}
+
+vocab_size: int = len(pdata)
+num_train_batch = len(pdata)//batch_size
+
+def load_train_func(index):
+    x, y = pdata[index]
+    return pdict[x], pdict[y], random.choices(range(len(pdict)), k=negative_sample_size)
+
+train_data_iter = data_iterator_simple(load_train_func, len(pdata), batch_size, shuffle=True, with_file_cache=False)
+
+
+"""
+"""
+
+def distance(u, v, eps=1e-5):
     uu = F.sum(F.pow_scalar(u, 2), axis=1)
-    vv = F.sum(F.pow_scalar(u, 2), axis=1)
+    vv = F.sum(F.pow_scalar(v, 2), axis=1)
     euclid_norm_pow2 = F.sum(F.pow_scalar(u - v, 2), axis=1)
-    return F.acosh(1 + 2 * euclid_norm_pow2 / ((1 - uu) * (1 - vv)))
+    alpha = F.maximum2(F.constant(eps, shape=uu.shape), 1.0 - uu)
+    beta = F.maximum2(F.constant(eps, shape=vv.shape), 1.0 - vv)
+
+
+    return F.acosh(1 + 2 * euclid_norm_pow2 / (alpha * beta))
+
+		# alpha, beta = max(self.eps,1-uu), max(self.eps,1-vv)
+		# gamma = max(1.,1+2*(uu-2*uv+vv)/alpha/beta)
+
 
 def projection(x: nn.NdArray, eps: float = 1e-5) -> nn.NdArray:
     return F.clip_by_norm(x, clip_norm=1-eps, axis=1)
@@ -65,19 +113,14 @@ class RiemannianSgd(object):
     
     def update(self):
         for key in self.params:
-            rescaled_gradient: nn.NdArray = self.params[key].grad * (1. - F.sum(self.params[key].data**2, axis=1))**2 / 4.
+            rescaled_gradient: nn.NdArray = self.params[key].grad * expand_dims((1. - F.sum(self.params[key].data**2, axis=1))**2 / 4., axis=-1)
             self.params[key].data -= self.lr * rescaled_gradient
             self.params[key].data = projection(self.params[key].data, eps=self.eps)
 
 def loss_function(u, v, negative_samples):
-    return F.sum(F.log(F.exp(-distance(u, v)) / sum([distance(u, x) for x in F.split(negative_samples, axis=2)])))
+    return F.sum(F.log(F.exp(-distance(u, v)) / sum([F.exp(-distance(u, x)) for x in F.split(negative_samples, axis=2)])))
 
 
-vocab_size: int = 10000
-embedding_size: int = 5
-batch_size: int = 128
-max_epoch: int = 100
-negative_sample_size = 5
 
 u = nn.Variable((batch_size,))
 v = nn.Variable((batch_size,))
@@ -91,3 +134,9 @@ _neg = F.transpose(_neg, axes=(0, 2, 1))
 loss = loss_function(_u, _v, _neg)
 
 nn.get_parameters()["embed/W"].d = I.UniformInitializer([-0.001, 0.001])(shape=(vocab_size, embedding_size))
+
+solver = RiemannianSgd(lr=0.01)
+solver.set_parameters(nn.get_parameters())
+
+trainer = Trainer(inputs=[u, v, negative_samples], loss=loss, solver=solver)
+trainer.run(train_data_iter, None, epochs=max_epoch)
